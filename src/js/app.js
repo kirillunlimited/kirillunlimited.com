fa65b413c02c6c78ab28b5cd5af573efb0e71d4d
Kirill Ivanov <kirillunlimited@gmail.com>
2021-10-15 23:54:47 +0300

    Update footer content

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,11 @@
 import { setupScheme } from './helpers/scheme';
 import { initAnalytics } from './helpers/analytics';
 
+const initCurrentYear = () => {
+  const currentYearElement = document.querySelector('#current-year');
+  currentYearElement.innerHTML = new Date().getFullYear();
+};
+
 setupScheme();
+initCurrentYear();
 initAnalytics();
