8614483bd71886c7d82a2262737f9160bd95acb4
Kirill Ivanov <kirillunlimited@gmail.com>
2021-12-08 23:45:29 +0300

    Add webpack-fix-style-only-entries

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const CssnanoPlugin = require('cssnano-webpack-plugin');
 const PostCSSPresetEnv = require('postcss-preset-env');
 const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
 
 const isDev = process.env.NODE_ENV !== 'production';
 
@@ -62,6 +63,7 @@ module.exports = {
     ],
   },
   plugins: [
+    new FixStyleOnlyEntriesPlugin(),
     new MiniCssExtractPlugin({ filename: `${baseFilename}.css` }),
     new WebpackManifestPlugin({ publicPath: '/assets/' }),
     ...(process.env.DEBUG ? [new BundleAnalyzerPlugin()] : []),
