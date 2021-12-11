ef36b35d3391e3582c61d3a10c969f89833e1c22
Kirill Ivanov <kirillunlimited@gmail.com>
2021-12-11 19:40:40 +0300

    Add html-minifier

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const htmlmin = require('html-minifier');
 
 const manifestPath = path.resolve(__dirname, 'dist/assets/manifest.json');
 
@@ -19,6 +20,24 @@ module.exports = function (config) {
           resolve(err ? `/assets/${name}` : JSON.parse(data)[name])
         );
       })
+  );
+
+  const shouldTransformHTML = (outputPath) =>
+    outputPath && outputPath.endsWith('.html') && process.env.NODE_ENV === 'production';
+
+  config.addTransform('htmlmin', (content, outputPath) =>
+    shouldTransformHTML(outputPath)
+      ? htmlmin.minify(content, {
+          html5: true,
+          removeComments: true,
+          collapseWhitespace: true,
+          collapseBooleanAttributes: true,
+          removeRedundantAttributes: true,
+          removeScriptTypeAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+          useShortDoctype: true,
+        })
+      : content
   );
 
   config.addPassthroughCopy('./src/assets/img');
