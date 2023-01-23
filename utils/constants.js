3a7481a48e4043abf68a7d0f68493d16aa67caf2
Ivanov Kirill <kirillunlimited@gmail.com>
2023-01-23 08:40:09 +0100

    Fix manifestPath

--- utils/constants.js
+++ utils/constants.js
@@ -2,5 +2,5 @@ const path = require('path');
 
 module.exports = {
   outputDir: 'dist',
-  manifestPath: path.resolve(__dirname, 'dist/assets/manifest.json'),
+  manifestPath: path.resolve(__dirname, '../dist/assets/manifest.json'),
 };
