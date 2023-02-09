e27f3d27af1e842b598bbb88232eccc8942daba3
Ivanov Kirill <kirillunlimited@gmail.com>
2023-02-09 17:25:47 +0100

    Sort posts

--- utils/collections.js
+++ utils/collections.js
@@ -1,13 +1,16 @@
 module.exports = {
   postsByYear: (collectionApi) => {
-    const dictByYear = collectionApi.getFilteredByTag('posts').reduce((dict, post) => {
-      const year = post.date.getFullYear();
-      if (!dict[year]) {
-        dict[year] = [];
-      }
-      dict[year] = [...dict[year], post];
-      return dict;
-    }, {});
+    const dictByYear = collectionApi
+      .getFilteredByTag('posts')
+      .sort((a, b) => b.date - a.date)
+      .reduce((dict, post) => {
+        const year = post.date.getFullYear();
+        if (!dict[year]) {
+          dict[year] = [];
+        }
+        dict[year] = [...dict[year], post];
+        return dict;
+      }, {});
     return Object.entries(dictByYear);
   },
 };
