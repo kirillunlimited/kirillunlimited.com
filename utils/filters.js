75a0f6cba37ee1f31a812c03971cf3bb27188a24
Kirill Ivanov <kirillunlimited@gmail.com>
2023-09-18 10:56:36 +0200

    Update date format

--- utils/filters.js
+++ utils/filters.js
@@ -4,7 +4,7 @@ module.exports = {
   sortByOrder: (elements) =>
     elements.filter((element) => element.data.permalink !== '/').sort((a, b) => a.data.order - b.data.order),
 
-  postDate: (dateObj) => DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED),
+  postDate: (dateObj) => DateTime.fromJSDate(dateObj).toFormat('dd LLL yyyy'),
 
   isPageInCollection: (collection = [], pageUrl = this.ctx.page.url) =>
     collection.some((element) => element.url === pageUrl),
