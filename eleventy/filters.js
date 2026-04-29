d3b41e2f3d7ce44b52157e46c91d8cf039cf4ec0
Kirill Ivanov <kirillunlimited@gmail.com>
2026-04-29 21:28:39 +0300

    Inline critical js

--- eleventy/filters.js
+++ eleventy/filters.js
@@ -1,3 +1,15 @@
 import { DateTime } from 'luxon';
+import fs from 'fs/promises';
+import { minify } from 'terser';
 
 export const postDate = (dateObj) => DateTime.fromJSDate(dateObj).toFormat('dd LLL yyyy');
+export const jsmin = async (filePath) => {
+  const code = await fs.readFile(filePath, 'utf-8');
+
+  const result = await minify(code, {
+    compress: true,
+    mangle: true,
+  });
+
+  return result.code;
+};
--- src/assets/js/init.js
+++ src/assets/js/init.js
@@ -1,7 +1,32 @@
-/**
- * Before render (critical)
- */
+(function () {
+  const SCHEMES = {
+    LIGHT: 'light',
+    AUTO: 'auto',
+    DARK: 'dark',
+  };
 
-import { initColorScheme } from './helpers/color-scheme';
+  const scheme = localStorage.getItem('scheme') || SCHEMES.AUTO;
 
-initColorScheme();
+  const lightStyles = document.querySelector('link[rel=stylesheet][data-color-scheme="light"]');
+  const darkStyles = document.querySelector('link[rel=stylesheet][data-color-scheme="dark"]');
+
+  let lightMedia, darkMedia;
+
+  switch (scheme) {
+    case SCHEMES.AUTO:
+      lightMedia = '(prefers-color-scheme: light)';
+      darkMedia = '(prefers-color-scheme: dark)';
+      break;
+    case SCHEMES.LIGHT:
+      lightMedia = 'all';
+      darkMedia = 'not all';
+      break;
+    case SCHEMES.DARK:
+      lightMedia = 'not all';
+      darkMedia = 'all';
+      break;
+  }
+
+  if (lightStyles) lightStyles.media = lightMedia;
+  if (darkStyles) darkStyles.media = darkMedia;
+})();
